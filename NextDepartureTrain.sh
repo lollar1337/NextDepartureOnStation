862ff90b678a577be49d94598be94ee0fe0579c7
/etc/Jade <77240684+thisjade@users.noreply.github.com>
2022-04-24 12:03:00 +0200

    Added Script

---
+++ NextDepartureTrain.sh
@@ -0,0 +1,7 @@
+raw_data=$(curl -s https://dbf.finalrewind.org/DS100.json?limit=1)
+
+destination=$(echo $raw_data | jq -r .departures[].destination)
+platform=$(echo $raw_data | jq -r .departures[].scheduledPlatform)
+train=$(echo $raw_data | jq -r .departures[].train)
+
+echo "Gleis" $platform "Einfahrt" $train "nach" $destination
